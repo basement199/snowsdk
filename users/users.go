e272b2863083ae8aaccf9d412a6e09fef710bf3c
basement199 <ffridiejr@gmail.com>
2025-01-08 22:05:23 -0500

    Remove create functions

--- users/users.go
+++ users/users.go
@@ -1,7 +1,6 @@
 package users
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -258,140 +257,59 @@ type SnowUser struct {
 		UExtEmplID                    string             `json:"u_ext_empl_id"`
 		UNotifyVip                    string             `json:"u_notify_vip"`
 		SysCreatedOn                  string             `json:"sys_created_on"`
-		SysDomain                     struct {
-			Link  string `json:"link"`
-			Value string `json:"value"`
-		} `json:"sys_domain"`
-		UItilUser                   string     `json:"u_itil_user"`
-		UWfqResourceID              string     `json:"u_wfq_resource_id"`
-		UBudgetCodes                string     `json:"u_budget_codes"`
-		UPhotoUploaded              string     `json:"u_photo_uploaded"`
-		HomePhone                   string     `json:"home_phone"`
-		UContProjectedEndDate       string     `json:"u_cont_projected_end_date"`
-		UEmployeeResourceGroups     string     `json:"u_employee_resource_groups"`
-		USeniorityDate              string     `json:"u_seniority_date"`
-		AverageDailyFte             string     `json:"average_daily_fte"`
-		UHrPersonnel                string     `json:"u_hr_personnel"`
-		UPimNumber                  string     `json:"u_pim_number"`
-		UUserid                     string     `json:"u_userid"`
-		UHrManagerPath              string     `json:"u_hr_manager_path"`
-		Name                        string     `json:"name"`
-		XViinBfCustAllCatalog       string     `json:"x_viin_bf_cust_all_catalog"`
-		FailedAttempts              string     `json:"failed_attempts"`
-		UCompanyDivision            string     `json:"u_company_division"`
-		Title                       string     `json:"title"`
-		SysID                       string     `json:"sys_id"`
-		UPwdNvrExprs                string     `json:"u_pwd_nvr_exprs"`
-		FederatedID                 string     `json:"federated_id"`
-		UDirExclude                 string     `json:"u_dir_exclude"`
-		InternalIntegrationUser     string     `json:"internal_integration_user"`
-		MobilePhone                 string     `json:"mobile_phone"`
-		Street                      string     `json:"street"`
-		UAltEmail                   string     `json:"u_alt_email"`
-		Company                     Company    `json:"company"`
-		Department                  Department `json:"department"`
-		UType                       string     `json:"u_type"`
-		FirstName                   string     `json:"first_name"`
-		UPreferredName              string     `json:"u_preferred_name"`
-		XMobiCExternalPhoneNumber   string     `json:"x_mobi_c_external_phone_number"`
-		Introduction                string     `json:"introduction"`
-		PreferredLanguage           string     `json:"preferred_language"`
-		UNotes                      string     `json:"u_notes"`
-		XMobiCExternalUserFirstName string     `json:"x_mobi_c_external_user_first_name"`
-		UCluster                    string     `json:"u_cluster"`
-		UNumber                     string     `json:"u_number"`
-		UGalName                    string     `json:"u_gal_name"`
-		UFloor                      string     `json:"u_floor"`
-		SysModCount                 string     `json:"sys_mod_count"`
-		UHumanMailbox               string     `json:"u_human_mailbox"`
-		ULockOutDate                string     `json:"u_lock_out_date"`
-		MiddleName                  string     `json:"middle_name"`
-		SysTags                     string     `json:"sys_tags"`
-		TimeZone                    string     `json:"time_zone"`
-		ULocalUser                  string     `json:"u_local_user"`
-		UWorkAtHome                 string     `json:"u_work_at_home"`
-		XMobiCMdmUserName           string     `json:"x_mobi_c_mdm_user_name"`
-		Location                    Location   `json:"location"`
-		UNameID                     string     `json:"u_name_id"`
-		UObjectguid                 string     `json:"u_objectguid"`
-		UAssignmentCategory         string     `json:"u_assignment_category"`
-	} `json:"result"`
-}
-
-type AddSnowUser struct {
-	UserName       string `json:"user_name"`
-	FirstName      string `json:"first_name"`
-	LastName       string `json:"last_name"`
-	Email          string `json:"email"`
-	Manager        string `json:"manager"`
-	EmployeeNumber string `json:"employee_number"`
-	Password       string `json:"password"`
-}
-
-type NewSnowUser struct {
-	Result struct {
-		CalendarIntegration    string `json:"calendar_integration"`
-		Country                string `json:"country"`
-		UserPassword           string `json:"user_password"`
-		LastLoginTime          string `json:"last_login_time"`
-		Source                 string `json:"source"`
-		SysUpdatedOn           string `json:"sys_updated_on"`
-		Building               string `json:"building"`
-		WebServiceAccessOnly   string `json:"web_service_access_only"`
-		Notification           string `json:"notification"`
-		EnableMultifactorAuthn string `json:"enable_multifactor_authn"`
-		SysUpdatedBy           string `json:"sys_updated_by"`
-		SysCreatedOn           string `json:"sys_created_on"`
-		SysDomain              struct {
-			Link  string `json:"link"`
-			Value string `json:"value"`
-		} `json:"sys_domain"`
-		State                   string `json:"state"`
-		Vip                     string `json:"vip"`
-		SysCreatedBy            string `json:"sys_created_by"`
-		Zip                     string `json:"zip"`
-		HomePhone               string `json:"home_phone"`
-		TimeFormat              string `json:"time_format"`
-		LastLogin               string `json:"last_login"`
-		DefaultPerspective      string `json:"default_perspective"`
-		Active                  string `json:"active"`
-		SysDomainPath           string `json:"sys_domain_path"`
-		CostCenter              string `json:"cost_center"`
-		Phone                   string `json:"phone"`
-		Name                    string `json:"name"`
-		EmployeeNumber          string `json:"employee_number"`
-		PasswordNeedsReset      string `json:"password_needs_reset"`
-		Gender                  string `json:"gender"`
-		City                    string `json:"city"`
-		FailedAttempts          string `json:"failed_attempts"`
-		UserName                string `json:"user_name"`
-		Roles                   string `json:"roles"`
-		Title                   string `json:"title"`
-		SysClassName            string `json:"sys_class_name"`
-		SysID                   string `json:"sys_id"`
-		FederatedID             string `json:"federated_id"`
-		InternalIntegrationUser string `json:"internal_integration_user"`
-		LdapServer              string `json:"ldap_server"`
-		MobilePhone             string `json:"mobile_phone"`
-		Street                  string `json:"street"`
-		Company                 string `json:"company"`
-		Department              string `json:"department"`
-		FirstName               string `json:"first_name"`
-		Email                   string `json:"email"`
-		Introduction            string `json:"introduction"`
-		PreferredLanguage       string `json:"preferred_language"`
-		Manager                 string `json:"manager"`
-		LockedOut               string `json:"locked_out"`
-		SysModCount             string `json:"sys_mod_count"`
-		LastName                string `json:"last_name"`
-		Photo                   string `json:"photo"`
-		Avatar                  string `json:"avatar"`
-		MiddleName              string `json:"middle_name"`
-		SysTags                 string `json:"sys_tags"`
-		TimeZone                string `json:"time_zone"`
-		Schedule                string `json:"schedule"`
-		DateFormat              string `json:"date_format"`
-		Location                string `json:"location"`
+		UItilUser                     string             `json:"u_itil_user"`
+		UWfqResourceID                string             `json:"u_wfq_resource_id"`
+		UBudgetCodes                  string             `json:"u_budget_codes"`
+		UPhotoUploaded                string             `json:"u_photo_uploaded"`
+		HomePhone                     string             `json:"home_phone"`
+		UContProjectedEndDate         string             `json:"u_cont_projected_end_date"`
+		UEmployeeResourceGroups       string             `json:"u_employee_resource_groups"`
+		USeniorityDate                string             `json:"u_seniority_date"`
+		AverageDailyFte               string             `json:"average_daily_fte"`
+		UHrPersonnel                  string             `json:"u_hr_personnel"`
+		UPimNumber                    string             `json:"u_pim_number"`
+		UUserid                       string             `json:"u_userid"`
+		UHrManagerPath                string             `json:"u_hr_manager_path"`
+		Name                          string             `json:"name"`
+		XViinBfCustAllCatalog         string             `json:"x_viin_bf_cust_all_catalog"`
+		FailedAttempts                string             `json:"failed_attempts"`
+		UCompanyDivision              string             `json:"u_company_division"`
+		Title                         string             `json:"title"`
+		SysID                         string             `json:"sys_id"`
+		UPwdNvrExprs                  string             `json:"u_pwd_nvr_exprs"`
+		FederatedID                   string             `json:"federated_id"`
+		UDirExclude                   string             `json:"u_dir_exclude"`
+		InternalIntegrationUser       string             `json:"internal_integration_user"`
+		MobilePhone                   string             `json:"mobile_phone"`
+		Street                        string             `json:"street"`
+		UAltEmail                     string             `json:"u_alt_email"`
+		Company                       Company            `json:"company"`
+		Department                    Department         `json:"department"`
+		UType                         string             `json:"u_type"`
+		FirstName                     string             `json:"first_name"`
+		UPreferredName                string             `json:"u_preferred_name"`
+		XMobiCExternalPhoneNumber     string             `json:"x_mobi_c_external_phone_number"`
+		Introduction                  string             `json:"introduction"`
+		PreferredLanguage             string             `json:"preferred_language"`
+		UNotes                        string             `json:"u_notes"`
+		XMobiCExternalUserFirstName   string             `json:"x_mobi_c_external_user_first_name"`
+		UCluster                      string             `json:"u_cluster"`
+		UNumber                       string             `json:"u_number"`
+		UGalName                      string             `json:"u_gal_name"`
+		UFloor                        string             `json:"u_floor"`
+		SysModCount                   string             `json:"sys_mod_count"`
+		UHumanMailbox                 string             `json:"u_human_mailbox"`
+		ULockOutDate                  string             `json:"u_lock_out_date"`
+		MiddleName                    string             `json:"middle_name"`
+		SysTags                       string             `json:"sys_tags"`
+		TimeZone                      string             `json:"time_zone"`
+		ULocalUser                    string             `json:"u_local_user"`
+		UWorkAtHome                   string             `json:"u_work_at_home"`
+		XMobiCMdmUserName             string             `json:"x_mobi_c_mdm_user_name"`
+		Location                      Location           `json:"location"`
+		UNameID                       string             `json:"u_name_id"`
+		UObjectguid                   string             `json:"u_objectguid"`
+		UAssignmentCategory           string             `json:"u_assignment_category"`
 	} `json:"result"`
 }
 
@@ -594,84 +512,4 @@ func (s *UserClient) GetAll() (SnowUser, error) {
 
 	return u, err
 
-}
-
-// CreateUser - Create a  New User
-func (s *UserClient) CreateUser(n AddSnowUser) (NewSnowUser, error) {
-
-	u := NewSnowUser{}
-
-	userJSON, err := json.Marshal(n)
-	if err != nil {
-		fmt.Println("Error marshaling user:", err)
-		return u, err
-	}
-
-	url := s.Instance + "/api/now/table/sys_user"
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(userJSON))
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return u, err
-	}
-
-	req.SetBasicAuth(s.Admin, s.AdminPassword)
-	req.Header.Set("Content-Type", "application/json")
-
-	// Make the HTTP request
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error making request:", err)
-		return u, err
-	}
-	defer resp.Body.Close()
-
-	err = json.NewDecoder(resp.Body).Decode(&u)
-
-	if err != nil {
-		return u, err
-	}
-
-	return u, err
-}
-
-// UpdateUser - update users
-func (s *UserClient) UpdateUser(id string, n AddSnowUser) (NewSnowUser, error) {
-
-	u := NewSnowUser{}
-
-	userJSON, err := json.Marshal(n)
-	if err != nil {
-		fmt.Println("Error marshaling user:", err)
-		return u, err
-	}
-
-	url := s.Instance + "/api/now/table/sys_user/" + id
-
-	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(userJSON))
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return u, err
-	}
-
-	req.SetBasicAuth(s.Admin, s.AdminPassword)
-	req.Header.Set("Content-Type", "application/json")
-
-	// Make the HTTP request
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error making request:", err)
-		return u, err
-	}
-	defer resp.Body.Close()
-
-	err = json.NewDecoder(resp.Body).Decode(&u)
-
-	if err != nil {
-		return u, err
-	}
-
-	return u, err
 }
